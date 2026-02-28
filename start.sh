ec0fe5526c3a5b276fb405923e30f351d6c11740
SamyBravy <samybravy@gmail.com>
2026-02-28 12:30:59 +0100

    Sistemata roba

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 echo https://quantum.cloud.ibm.com && \
 docker build -t ft_quantum_img . && \
-docker run -it -p 8888:8888 --env-file .env -v /home/sdell-er/Desktop/Progetti/Mastery/Ftl_quantum:/app ft_quantum_img
+docker run -it -p 8888:8888 --env-file .env -v $PWD:/app ft_quantum_img
